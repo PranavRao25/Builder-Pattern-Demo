76212a082ccfe5b89082652401651d11854b07d1
Pranav Guruprasad Rao <pranavrao2500@gmail.com>
2024-08-30 22:34:18 +0530

    Some Bug fixes

---
+++ Processing/ConvertibleBuilder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Processing
+{
+    /// <summary>
+    /// Builder for a Convertible Car
+    /// </summary>
+    public class ConvertibleBuilder : IBuilder
+    {
+        private CarProduct ConvertibleCar;
+
+        /// <summary>
+        /// Builds a Convertible Car
+        /// </summary>
+        public void BuildCar()
+        {
+            ConvertibleCar = new CarProduct();
+            ConvertibleCar.SetBootCapacity(0);
+            ConvertibleCar.SetSeatingCapacity(2);
+            ConvertibleCar.SetWheelDrive(4);
+            ConvertibleCar.SetEngineSize(1.2);
+            ConvertibleCar.SetEngineType("V8");
+            ConvertibleCar.SetHorsePower(120);
+            ConvertibleCar.SetMaximumSpeed(140);
+            ConvertibleCar.SetNumberOfGears(6);
+        }
+
+        /// <summary>
+        /// Returns a constructed Convertible Car
+        /// </summary>
+        public CarProduct GetResult()
+        {
+            return ConvertibleCar;
+        }
+    }
+}
---
+++ Processing/PickupBuilder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Processing
+{
+    /// <summary>
+    /// Builder for a Pickup Car
+    /// </summary>
+    public class PickupBuilder : IBuilder
+    {
+        private CarProduct PickupCar;
+
+        /// <summary>
+        /// Builds a Pickup Car
+        /// </summary>
+        public void BuildCar()
+        {
+            PickupCar = new CarProduct();
+            PickupCar.SetBootCapacity(20);
+            PickupCar.SetSeatingCapacity(2);
+            PickupCar.SetWheelDrive(4);
+            PickupCar.SetEngineSize(1);
+            PickupCar.SetEngineType("V8");
+            PickupCar.SetHorsePower(120);
+            PickupCar.SetMaximumSpeed(60);
+            PickupCar.SetNumberOfGears(5);
+        }
+
+        /// <summary>
+        /// Returns a constructed Pickup Car
+        /// </summary>
+        public CarProduct GetResult()
+        {
+            return PickupCar;
+        }
+    }
+}
