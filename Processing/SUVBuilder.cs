e353241b7700e901bdc840d3477f30ad452ff477
Pranav Guruprasad Rao <pranavrao2500@gmail.com>
2024-09-03 21:01:34 +0530

    Update SUVBuilder.cs

--- Processing/SUVBuilder.cs
+++ Processing/SUVBuilder.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿/******************************************************************************
+ * Filename    = SUVBuilder.cs
+ *
+ * Author      = Pranav Guruprasad Rao
+ *
+ * Product     = BuilderPatternDemo
+ * 
+ * Project     = Processing
+ *
+ * Description = Builder for SUV Car
+ *****************************************************************************/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
