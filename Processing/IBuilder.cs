c864c559052efb5d525a297f6b7ca5abeca271bb
Pranav Guruprasad Rao <pranavrao2500@gmail.com>
2024-09-03 21:00:06 +0530

    Update IBuilder.cs

--- Processing/IBuilder.cs
+++ Processing/IBuilder.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿/******************************************************************************
+ * Filename    = IBuilder.cs
+ *
+ * Author      = Pranav Guruprasad Rao
+ *
+ * Product     = BuilderPatternDemo
+ * 
+ * Project     = Processing
+ *
+ * Description = Interface for the Builder for Car Product
+ *****************************************************************************/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
